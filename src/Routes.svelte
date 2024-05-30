aa59af9b6058f82ac5d5355f7b2d8cb4a34758e4
JoaoCardoso193 <joaocardoso193@gmail.com>
2024-05-30 21:45:42 +0700

    bkp

---
+++ src/Routes.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Route } from "tinro";
+  import HomePage from "./HomePage.svelte";
+  import TestPage from "./TestPage.svelte";
+</script>
+
+<Route path="/">
+  <HomePage />
+</Route>
+<Route path="/test">
+  <TestPage />
+</Route>
