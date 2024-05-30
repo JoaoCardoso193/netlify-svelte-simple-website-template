e972a2efe87ccf1b92523ae5313cf997aa370dd8
JoaoCardoso193 <joaocardoso193@gmail.com>
2024-05-30 21:57:49 +0700

    basic routing and hot reload setup

--- src/Routes.svelte
+++ src/Routes.svelte
@@ -2,6 +2,7 @@
   import { Route } from "tinro";
   import HomePage from "./HomePage.svelte";
   import TestPage from "./TestPage.svelte";
+  import InvalidRoute from "./404.svelte";
 </script>
 
 <Route path="/">
@@ -9,4 +10,7 @@
 </Route>
 <Route path="/test">
   <TestPage />
+</Route>
+<Route fallback>
+  <InvalidRoute />
 </Route>
